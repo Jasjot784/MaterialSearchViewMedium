2dcb98701ca4754f2d13cc7277c1438b342415e9
Jasjot Singh <jasjot784@gmail.com>
2020-07-22 22:00:52 +0530

    Use library in existing state

--- app/src/main/java/com/jasjotsingh/materialsearchview/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/materialsearchview/MainActivity.java
@@ -3,12 +3,27 @@ package com.jasjotsingh.materialsearchview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MaterialSearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.setSuggestions(getResources().getStringArray(R.array.query_suggestions));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
     }
 }
